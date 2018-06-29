33ef40952bc076e68f8c702c2d0fcc41422311e8
AAslsop12 <aimee.johansen33@gmail.com>
2018-06-29 09:09:36 -0600

    managed the sign up response

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -2,14 +2,16 @@ import axios from 'axios';
 import { ROOT_URL } from '../config';
 
 export function signUp(fields, success) {
-    console.log(field);
+    console.log(fields);
+
     return function(dispatch) {
-        // axios.post(`${ROOT_URL}/signUp`, fields)
-        //     .then(response => {
-        //         //do something
-        //     })
-        //     .catch(err => {
-        //         if(err) { console.log(err) }
-        //     })
+        axios.post(`${ROOT_URL}/signUp`, fields)
+            .then(response => {
+               console.log(response);
+               success()
+            })
+            .catch(err => {
+                if(err) { console.log(err) }
+            })
     }
 }

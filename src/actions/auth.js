5ef724b49a0b4f24f22505b85c9d9cb65290c867
AAslsop12 <aimee.johansen33@gmail.com>
2018-06-29 11:54:12 -0600

    storing user data from the sign-up post request, authenticate user reducer

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,13 +1,18 @@
 import axios from 'axios';
 import { ROOT_URL } from '../config';
 
-export function signUp(fields, success) {
-    console.log(fields);
+import {
+    AUTHENTICATE_USER
+} from './types';
 
+export function signUp(fields, success) {
     return function(dispatch) {
         axios.post(`${ROOT_URL}/signUp`, fields)
             .then(response => {
-               console.log(response);
+               dispatch({
+                   type: AUTHENTICATE_USER,
+                   payload: response.data
+               })
                success()
             })
             .catch(err => {

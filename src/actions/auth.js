0611fcbe007c3b766548410b39bac1406166c29f
AAslsop12 <aimee.johansen33@gmail.com>
2018-06-29 08:32:38 -0600

    installed redux-thunk and axios. built auth actions

---
+++ src/actions/auth.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import { ROOT_URL } from '../config';
+
+export function signUp(fields, success) {
+    console.log(field);
+    return function(dispatch) {
+        // axios.post(`${ROOT_URL}/signUp`, fields)
+        //     .then(response => {
+        //         //do something
+        //     })
+        //     .catch(err => {
+        //         if(err) { console.log(err) }
+        //     })
+    }
+}

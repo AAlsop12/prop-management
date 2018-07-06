3aaf7e2c7efbb074f0ee6e4d14c9afaca66c4754
AAslsop12 <aimee.johansen33@gmail.com>
2018-07-06 12:31:56 -0600

    customized newsletter new form

--- src/components/formFields.js
+++ src/components/formFields.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
+
 export class FormInput extends Component {
     render() {
         const { className, title, input, type, placeholder } = this.props;
-
         return (
             <div className={`${className} form-input`}>
                 <label className='form-input__title'>{title}</label>
@@ -11,27 +11,46 @@ export class FormInput extends Component {
                     className='form-input__input'
                     type={type}
                     {...input}
-                    placeholder={placeholder}
+                    placeholder={placeholder}    
                 />
             </div>
         )
     }
 }
 
+export class FormTextArea extends Component {
+    render() {
+        const { className, title, input, type, placeholder } = this.props;
+        return (
+            <div className={`${className} form-textarea`}>
+                <label className='form-textarea__title'>{title}</label>
+                <textarea
+                    className='form-textarea__input'
+                    type={type}
+                    {...input}
+                    placeholder={placeholder} 
+
+                   
+                >
+                
+                </textarea>
+            </div>
+        )
+    }
+}
+
+
 export class FormButton extends Component {
     render() {
         const { className, title, input, type } = this.props;
-
         return (
             <div className={`${className} form-button`}>
-
                 <button
-
-                    className={`form-button__button`}
+                    className='form-button__button'
                     type={type}
-                    {...input}
+                    {...input} 
                 >
-                    {title}
+                {title}
                 </button>
             </div>
         )

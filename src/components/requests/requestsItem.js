69f5e82c99cd7d8b8ceb60cb8665f4fa41344219
AAslsop12 <aimee.johansen33@gmail.com>
2018-07-20 08:05:03 -0600

    installed react-animate-height

--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -1,40 +1,64 @@
-import React, { Component } from 'react' ;
+import React, { Component } from 'react';
 
 import Icon from '../icon';
 import Button from '../button';
 
+import AnimateHeight from 'react-animate-height';
+
 class RequestsItem extends Component {
     
+    constructor() {
+        super()
+
+        this.state = {
+            height: 0
+        }
+    }
+
+    toggleDropdown = () => {
+        if(this.state.height == 0) {
+            this.setState({height: 'auto'})
+        } else {
+            this.setState({height: 0})
+        }
+    }
+
     render() {
         return (
             <div className='requests-item'>
-                    <Icon className='requests-item__icon'icon='fas fa-exclamation-triangle'/>
-                    <div className='requests-item__title'>
-                        <div className='requests-item__title__text'>Fix my sprinkler!</div>
-                        <Icon className='requests-item__title__arrow' icon='fas fa-sort-down'/>
-                    </div>
-                    <div className='requests-item__tenant-unit'>
-                        Max - Unit 335
-                    </div>
-                    <div className='requests-item__date'>
-                        10/10/1991
-                    </div>
-                    <Button className='requests-item__move' icon='fas fa-wrench' callback={() => console.log('tryna change request item status')}/>
-                    
-                    <div className='requests-item__description'>
-                        <img 
-                            className='requests-item__description.img'
-                            src='http://via.placeholder.com/160x94'
-                            />
-                        <p className='requests-item__description-text'>
-                            lorem ipsum gind oaindfasdoin adoignasdofin 
-                            lorem ipsum gind oaindfasdoin adoignasdofin 
-                            lorem ipsum gind oaindfasdoin adoignasdofin 
-                            lorem ipsum gind oaindfasdoin adoignasdofin 
+                <Icon className='requests-item__icon' icon='fas fa-exclamation-triangle'/>
+                <div className='requests-item__title'>
+                    <div className='requests-item__title__text'>Yo my door fell down</div>
+                    <Icon callback={() => this.toggleDropdown()} className='requests-item__title__arrow' icon='fas fa-sort-down'/>
+                </div>
+                <div className='requests-item__tenant-unit'>
+                    Max - Unit 115
+                </div>
+                <div className='requests-item__date'>
+                    09/15/97
+                </div>
+                <Button className='requests-item__move' icon='fas fa-wrench' callback={() => console.log('tryna change request status')}/>
 
-                        
-                        </p>
-                    </div>
+                <div className='requests-item__description'>
+                    <AnimateHeight
+                        duration={300}
+                        height={this.state.height}
+                    >
+                        <div className='requests-item__description'>
+                            <img 
+                                className='requests-item__description-img'
+                                src='http://via.placeholder.com/160x94'                        
+                            />
+                            <p className='requests-item__description-text'>
+                                Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+                                Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+                                Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+                                Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+                            </p>
+                        </div>
+                    </AnimateHeight>
+                </div>
+
             </div>
         )
     }

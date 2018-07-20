bf3faef44e47962d491ad1d7c4898485435671f9
AAslsop12 <aimee.johansen33@gmail.com>
2018-07-19 21:32:59 -0600

    cleaned up requestItem grid

--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -10,12 +10,12 @@ class RequestsItem extends Component {
             <div className='requests-item'>
                     <Icon className='requests-item__icon'icon='fas fa-exclamation-triangle'/>
                     <div className='requests-item__title'>
-                        Fix my sprinkler!
+                        <div className='requests-item__title__text'>Fix my sprinkler!</div>
+                        <Icon className='requests-item__title__arrow' icon='fas fa-sort-down'/>
                     </div>
                     <div className='requests-item__tenant-unit'>
                         Max - Unit 335
                     </div>
-                    <Icon className='requests-item__arrow' icon='fas fa-sort-down'/>
                     <div className='requests-item__date'>
                         10/10/1991
                     </div>

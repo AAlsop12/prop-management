8fb340edfbda61b4b15f1924a088b30276431545
AAslsop12 <aimee.johansen33@gmail.com>
2018-07-19 19:55:56 -0600

    added icon component

--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react' ;
 
+import Icon from '../icon';
+
 class RequestsItem extends Component {
     
     render() {
         return (
             <div className='requests-item'>
-                Here be we item
+                <div>
+                    <Icon icon='fas fa-exclamation-triangle'/>
+                </div>
             </div>
         )
     }

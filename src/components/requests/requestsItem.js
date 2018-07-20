b964384aef50781f49c6210f2367a908356deb4b
AAslsop12 <aimee.johansen33@gmail.com>
2018-07-19 19:43:48 -0600

    built a request item component

---
+++ src/components/requests/requestsItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react' ;
+
+class RequestsItem extends Component {
+    
+    render() {
+        return (
+            <div className='requests-item'>
+                Here be we item
+            </div>
+        )
+    }
+}
+
+export default RequestsItem;
